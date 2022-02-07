3c3ff1d86a5268bbdcc06baa2e43acbac6a6a2fc
Victoria Haber <vhaber@pratt.edu>
2022-02-07 10:33:57 -0500

    update
    update new coding hw

---
+++ codehw1.php
@@ -0,0 +1,25 @@
+<html>
+<body>
+<?php
+
+
+
+$change = 159;
+$doll = "1 dollar";
+$quart = "two quarters";
+$nick = "one nickel";
+$pen = "three pennys";
+
+echo "You get 159 total cents in change from a store"."<br>"."The store gave you: $doll, $quart, $nick and $pen. <br> <br>";
+
+
+$bottles = 99;
+while ($bottles > 0){
+  echo "$bottles bottles of beer on the wall, ";
+  echo "$bottles bottles of beer, ";
+  echo "take one down and pass it around </br></br> ";
+  $bottles--;
+}
+?>
+</body>
+</html>
--- first.php
+++
@@ -1,7 +0,0 @@
-<html>
-<body>
-<?php
-echo "Hello World! This is an update";
-?>
-</body>
-</html>
